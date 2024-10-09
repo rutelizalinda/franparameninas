1108c418f7c22754f7e2b5a0b7e979a89009fda1
rutelizalinda <143123214+rutelizalinda@users.noreply.github.com>
2024-10-09 08:47:27 -0300

    hjxbv

--- perguntas.js
+++ perguntas.js
@@ -9,5 +9,80 @@ criaCartao(
     'Como se calcula a soma dos termos de uma P.A?',
     'Utiliza-se a fórmula: ((a1+an)*r)/2'
 
- cr   
-)
+ criaCartao(
+ 'ciências',
+ 'O que significa DNA?',
+ 'Ácido desoxirribonucleico',
+ 
+ )
+
+ criaCartao(
+'ciências',
+'Qual é o tipo de sangue mais raro?',
+'AB negativo',
+
+ )
+ 
+ criaCartao(
+'ciências',
+'A terra tem três camadas quais sao elas?',
+'Crosta, manto e núcleo',
+
+ )
+
+ criaCartao(
+'conhecimentos gerais',
+'qual o menor país do mundo?',
+'Rússia',
+
+ )
+
+ criaCartao(
+'conhecimentos gerais',
+'Quem inventou a lâmpada?',
+'Thomas Edison',
+
+ )
+
+ criaCartao(
+'conhecimentos gerais',
+'Quais são as fases da Lua?',
+'Nova, crescente, cheia e minguante',
+
+ )
+
+ criaCartao(
+'ciências',
+'Nova, crescente, cheia e minguante',
+'100 ºC',
+
+ )
+
+ criaCartao(
+'conhecimentos gerais',
+'Quantos ossos temos no nosso corpo?',
+'206',
+
+ )
+
+ criaCartao(
+'conhecimento gerais',
+'Qual o maior planeta do sistema solar?',
+'Júpiter',
+
+ )
+
+ criaCartao(
+'conhecimentos gerais',
+'Um anel tem 3 pedras preciosas. Quantas pedras preciosas têm 11 anéis?',
+'33.',
+
+ )
+
+ criaCartao(
+'conhecimentos gerais',
+' Quantos continentes existem?',
+'6',
+
+ )
+ 
