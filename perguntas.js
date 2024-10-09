e2dd7c0aa1bfa6ba9415a4c6f8868cab3c4060cd
rutelizalinda <143123214+rutelizalinda@users.noreply.github.com>
2024-10-09 08:15:39 -0300

    gjwg

---
+++ perguntas.js
@@ -0,0 +1,13 @@
+criaCartao(
+    'Matemática'
+    'Como se calcula a soma dos termos de uma P.A?',
+    'Utiliza-se a fórmula: ((a1+an)*r)/2'
+)
+
+criaCartao(
+    'Matemática',
+    'Como se calcula a soma dos termos de uma P.A?',
+    'Utiliza-se a fórmula: ((a1+an)*r)/2'
+
+ cr   
+)
