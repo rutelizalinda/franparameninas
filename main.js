2222a0eb1e02d5e4e454b7188fb9bc898b77ba0e
rutelizalinda <143123214+rutelizalinda@users.noreply.github.com>
2024-10-16 08:05:57 -0300

    gfj

---
+++ main.js
@@ -0,0 +1,17 @@
+function criaCartao(categoria, pergunta, resposta) {
+    Cartao.className = 'cartao';
+    cartao.innerHTML =` 
+<div class="conteudo-cartao">
+   <h3>categoria</h3>
+<div class="pergunta-cartao">
+    <p>pergunta</p>
+
+ </div>
+ <div class="resposta-cartao">
+     <p>reposta</p>
+  </div>
+</div>
+`
+
+    
+}
