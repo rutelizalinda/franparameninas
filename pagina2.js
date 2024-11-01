c084ef1a0f120a7318fb402db9e0fcb0af360946
rutelizalinda <143123214+rutelizalinda@users.noreply.github.com>
2024-11-01 08:25:57 -0300

    cty

--- pagina2.js
+++ pagina2.js
@@ -3,7 +3,7 @@ function criaCartao(categoria, pergunta, resposta) {
     let cartao = document.createElement('article');
     cartao.className = 'cartao';
     cartao.innerHTML =` 
-<div class="conteudo-cartao">
+<div class="conteudo-cartao" onclik="mostrarResposta (this)">
    <h3>${categoria}</h3>
 <div class="pergunta-cartao">
     <p>${pergunta}</p>
@@ -16,6 +16,19 @@ function criaCartao(categoria, pergunta, resposta) {
 container.appendChild(cartao);
     
 }
-function mostrarResposta(){
+function mostrarResposta(card){
     const resposta = card.querySelector('.resposta-cartao');
 }
+
+    if(resposta.style.display === 'none') {
+    resposta.style.display == 'block';
+
+  }
+   else{
+    resposta.style.display = 'none';
+
+   }
+
+
+    
+   
